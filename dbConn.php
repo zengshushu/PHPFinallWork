12c2125d087e33e245593953e97f9d7d4bf9114d
zengshushu <709189913@qq.com>
2019-04-25 13:43:08 +0800

    注册进本地数据库

---
+++ dbConn.php
@@ -0,0 +1,16 @@
+﻿<?php  
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PWD', '');
+  define('DB_CHARSET', 'UTF8');
+  define('DB_DBNAME', 'neuvideo');
+  function connect (){ 
+    //连接mysql
+    $link=@mysql_connect(DB_HOST,DB_USER,DB_PWD) or die ('数据库连接失败<br/>ERROR '.mysql_errno().':'.mysql_error());
+    //设置字符集
+    mysql_set_charset(DB_CHARSET);
+    //打开指定的数据库
+    mysql_select_db(DB_DBNAME) or die('指定的数据库打开失败'.mysql_error());
+    return $link;
+ }  
+ ?>
